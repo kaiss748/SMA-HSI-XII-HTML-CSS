8bbd86f27969cae7aca42f00ee34f2e65c69dc9c
kaiss748 <yubelkaiii@gmail.com>
2025-09-25 14:50:08 +0700

    eaaa

---
+++ Kartu/eaaa.js
@@ -0,0 +1,15 @@
+const timA = document.querySelector("#skor-tim-a");
+const timB = document.querySelector("#skor-tim-b");
+
+timA.innerHTML = "15";
+timB.innerHTML = "10";
+
+const tittle = document.querySelector("#judul");
+tittle.style.color = "blue";
+tittle.style.textAlign = "center";
+
+const lampuKaka = document.querySelector("#lampu-ajaib");
+lampuKaka.style.backgroundColor = "yellow";
+lampuKaka.style.color = "green"
+lampuKaka.style.borderColor = "orange"
+lampuKaka.innerHTML = "Lampu menyala!"
