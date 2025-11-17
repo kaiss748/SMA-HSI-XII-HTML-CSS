9db5d2761ca81815411bfb1a3db61fa243353a85
kaiss748 <yubelkaiii@gmail.com>
2025-11-17 21:15:32 +0700

    otak-atik

--- Project_2/app.py
+++ Project_2/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
 semua_pesan = []
+
+app.secret_key = 'ini-rahasia-banget-loh'
 
 @app.route('/guestbook', methods=['GET', 'POST'])
 def bukutamu():
@@ -11,7 +13,11 @@ def bukutamu():
         pesan = request.form['pesan_post']
         komen = {'nama': nama, 'pesan': pesan}
         semua_pesan.append(komen)
-        return render_template('GuestBook.html', komen=komen, nama=nama, pesan=pesan, semua_pesan=semua_pesan)
+
+        flash('Pesan Anda telah berhasil dikirim!', 'success') # 'success' adalah kategori (opsional)
+
+        return redirect(url_for('bukutamu'))
+        # return render_template('GuestBook.html', komen=komen, nama=nama, pesan=pesan, semua_pesan=semua_pesan)
     else:
         return render_template('GuestBook.html', semua_pesan=semua_pesan)
     
